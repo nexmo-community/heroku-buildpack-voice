d3642ed84071653b98c91db1ab9e561488fd49ce
BuildTools <sam@sammachin.com>
2017-03-03 14:27:10 +0000

    fix

--- postDeploy.sh
+++ postDeploy.sh
@@ -14,6 +14,6 @@ name = "Heroku - $HEROKU_APP_NAME"
 url = "https://$HEROKU_APP_NAME.herokuapp.com"
 
 echo "-----> Updating Nexmo App Endpoints"
-curl -X PUT "https://api.nexmo.com/v1/applicationsapp_id?api_key=$api_key&api_secret=$api_secret&name=$name&type=voice&answer_url=$url/ncco&event_url=$url/event"
+curl -X PUT "https://api.nexmo.com/v1/applications/$app_id?api_key=$api_key&api_secret=$api_secret&name=$name&type=voice&answer_url=$url/ncco&event_url=$url/event"
 
 echo "-----> Nexmo App configured"
